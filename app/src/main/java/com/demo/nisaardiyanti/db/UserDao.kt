c83eeaf833673d9e4127deacac9d949218686f71
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-10-06 10:58:51 +0700

    Update UserDao.kt

--- app/src/main/java/com/demo/nisaardiyanti/db/UserDao.kt
+++ app/src/main/java/com/demo/nisaardiyanti/db/UserDao.kt
@@ -1,5 +1,5 @@
 package com.demo.nisaardiyanti.db
-
+//nama packages
 import androidx.room.*
 
 @Dao
@@ -8,15 +8,15 @@ interface UserDao {
 
     @Query("SELECT * FROM userinfo ORDER BY id DESC")
     fun getAllUserInfo(): List<UserEntity>?
-
+//menampilkan data dari userinfo
 
     @Insert
     fun insertUser(user: UserEntity?)
-
+//funtion insertuser
     @Delete
     fun deleteUser(user: UserEntity?)
-
+//function deleteuser
     @Update
     fun updateUser(user: UserEntity?)
-
+//function updateuser
 }
