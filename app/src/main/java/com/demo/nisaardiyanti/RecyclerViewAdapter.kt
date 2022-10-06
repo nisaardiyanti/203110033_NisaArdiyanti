c41bd4b575db83af8990a2d9412140a95d1bb713
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-10-06 11:14:41 +0700

    Update RecyclerViewAdapter.kt

--- app/src/main/java/com/demo/nisaardiyanti/RecyclerViewAdapter.kt
+++ app/src/main/java/com/demo/nisaardiyanti/RecyclerViewAdapter.kt
@@ -1,5 +1,5 @@
 package com.demo.nisaardiyanti
-
+//nama packages
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +8,13 @@ import com.demo.nisaardiyanti.db.UserEntity
 import kotlinx.android.synthetic.main.recyclerview_row.view.*
 
 class RecyclerViewAdapter(val listener: RowClickListener): RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
-
+//kelas RecyclerViewAdapter
     var items  = ArrayList<UserEntity>()
 
     fun setListData(data: ArrayList<UserEntity>) {
         this.items = data
     }
-
+//untuk membuat daftar array pada userentity
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.MyViewHolder {
        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_row, parent, false)
         return MyViewHolder(inflater, listener)
@@ -36,12 +36,13 @@ class RecyclerViewAdapter(val listener: RowClickListener): RecyclerView.Adapter<
 
 
     class MyViewHolder(view: View, val listener: RowClickListener): RecyclerView.ViewHolder(view) {
-
+//class MyViewHolder dengan berisi val tvName view.Name,view.tvMail,view.tvPhone,view.deleteUserID
         val tvName = view.tvName
         val tvEmail = view.tvEmail
         val tvPhone = view.tvPhone
         val deleteUserID = view.deleteUserID
 
+         //Data binding dengan berisi data.name,data.email,data.phone
         fun bind(data: UserEntity) {
             tvName.text = data.name
 
@@ -53,6 +54,9 @@ class RecyclerViewAdapter(val listener: RowClickListener): RecyclerView.Adapter<
             deleteUserID.setOnClickListener {
                 listener.onDeleteUserClickListener(data)
             }
+                //Listener merupakan kelas abstrak atau antarmuka yang digunakan untuk
+            // menyediakan fungsionalitas untuk komponen UI yang dapat berinteraksi.
+                //pada kode diatas delete akan dapat berfungsi karena sudah diset On Click
         }
     }
 
