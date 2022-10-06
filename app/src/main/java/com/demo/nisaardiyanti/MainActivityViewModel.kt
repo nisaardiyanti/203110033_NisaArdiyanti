69fce51302c3d6f603a83a7874e9410870d91b17
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-10-06 11:03:09 +0700

    Update MainActivityViewModel.kt

--- app/src/main/java/com/demo/nisaardiyanti/MainActivityViewModel.kt
+++ app/src/main/java/com/demo/nisaardiyanti/MainActivityViewModel.kt
@@ -1,11 +1,11 @@
 package com.demo.nisaardiyanti
-
+//nama packages
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.demo.nisaardiyanti.db.RoomAppDb
 import com.demo.nisaardiyanti.db.UserEntity
-
+//kelas MainActivityViewModel
 class MainActivityViewModel(app: Application): AndroidViewModel(app) {
     lateinit var allUsers : MutableLiveData<List<UserEntity>>
 
@@ -13,15 +13,17 @@ class MainActivityViewModel(app: Application): AndroidViewModel(app) {
         allUsers = MutableLiveData()
         getAllUsers()
     }
+    ////inisiasi dari allusers
 
     fun getAllUsersObservers(): MutableLiveData<List<UserEntity>> {
         return allUsers
     }
+    // funtion untuk mengembalikan allusers
 
     fun getAllUsers() {
         val userDao = RoomAppDb.getAppDatabase((getApplication()))?.userDao()
         val list = userDao?.getAllUserInfo()
-
+//funtion getAllUsers terdiri  dari val userDao dengan data berasal dari RoomAppDb.getAppDatabase dan val list mengambil dari getUserInfo
         allUsers.postValue(list)
     }
 
@@ -30,6 +32,7 @@ class MainActivityViewModel(app: Application): AndroidViewModel(app) {
         userDao?.insertUser(entity)
         getAllUsers()
     }
+    //funtion menambahkan userinfo dengan val userDao dari data RoomAppDatabase
 
     fun updateUserInfo(entity: UserEntity){
         val userDao = RoomAppDb.getAppDatabase(getApplication())?.userDao()
